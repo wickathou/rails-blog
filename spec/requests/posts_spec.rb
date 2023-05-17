48a0784462f0091a863c3542e6efaf6c23b486b0
Javier <javidx@gmail.com>
2023-05-17 13:21:34 +0300

    Updated expect blocks to check if response.body includes the content expected

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Posts', type: :request do
       get '/users/1/posts/'
       expect(response).to have_http_status(:success)
       expect(response).to render_template('posts/index')
+      expect(response.body).to include('Posts#index')
       assert_select 'h1', 'Posts#index'
     end
   end
@@ -15,6 +16,7 @@ RSpec.describe 'Posts', type: :request do
       get '/users/1/posts/1'
       expect(response).to have_http_status(:success)
       expect(response).to render_template('posts/show')
+      expect(response.body).to include('Posts#show')
       assert_select 'h1', 'Posts#show'
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,13 +6,16 @@ RSpec.describe 'Users', type: :request do
       get '/users/'
       expect(response).to have_http_status(:success)
       expect(response).to render_template('users/index')
+      expect(response.body).to include('Users')
       assert_select 'h1', 'Users'
     end
-
+  end
+  describe 'GET /show' do
     it 'returns http success' do
       get '/users/1'
       expect(response).to have_http_status(:success)
       expect(response).to render_template('users/show')
+      expect(response.body).to include('User data')
       assert_select 'h2', 'User data'
     end
   end
